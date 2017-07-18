d3ccf4956a01e4b2808a8d71d1909b2a16ff6c37
JB-Tellez <jb.tellez@gmail.com>
2017-07-18 12:22:47 +0200

    additional routes to filter posts, category seeds

---
+++ bin/seed-categories.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+require("dotenv").config();
+mongoose.connect(process.env.MONGODB_URI); // db name from local .env
+mongoose.connect(process.env.MONGODB_ADMIN_PWD); // admin password from local .env
+const Category = require('../models/category');
+
+const categories = [
+    {
+        name: 'Awesomes',
+        imageUrl: '',
+        description: ''
+    },
+    {
+        name: 'Docs',
+        imageUrl: '',
+        description: ''
+    },
+]
+
+Category.create(categories, (err, docs) => {
+  if (err) {
+    throw err;
+  }
+  docs.forEach((category) => {
+    console.log(category.name)
+  });
+  mongoose.connection.close();
+});
