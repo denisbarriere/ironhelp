9efaac1a892aaa9413adeb6ac395ac57e7138e64
Denis Barriere <denis.barriere@gmail.com>
2017-07-18 11:36:38 +0200

    Show different navbar color based on user role

--- helpers/auth.js
+++ helpers/auth.js
@@ -2,10 +2,17 @@ module.exports = {
 	setCurrentUser: function(req, res, next) {
 	  if (req.session.passport) {
 	    res.locals.currentUser = req.session.passport;
-	    res.locals.isUserLoggedIn = true;
+			res.locals.isUserLoggedIn = true;
+			
+			// if the user is an admin, set a flag to true
+			res.locals.isUserAdmin = false;
+
+			if (res.locals.currentUser.user.role === 'ADMIN') { res.locals.isUserAdmin = true; }
+	
 	  } else {
 	  	// delete res.locals.currentUser;
-	    res.locals.isUserLoggedIn = false;
+			res.locals.isUserLoggedIn = false;
+			res.locals.isUserAdmin = false;
 	  }
 	  next();
 	},
