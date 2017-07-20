c76eb62d8ba42bdad5a7e62158977934c141fee4
Denis Barriere <denis.barriere@gmail.com>
2017-07-20 21:17:14 +0200

    Add default images to collection

--- models/category.js
+++ models/category.js
@@ -3,7 +3,10 @@ const Schema = mongoose.Schema;
 
 const schema = new Schema({
     name: String,
-    imageUrl: String,
+    imageUrl: {
+        type: String,
+        default: 'http://bulma.io/images/placeholders/128x128.png'
+    },
     description: String,
 });
 
